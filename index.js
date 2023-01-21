d6b3b1814fbdc97363aaea6f227d8b5b0258d721
Romulo91 <romulo.galiao@googlemail.com>
2023-01-21 16:13:21 +0100

    firs backend setup done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./models/socket');
+
+require('./services/passport');
+
+mongoose.set('strictQuery', true);
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5500;
+server.listen(PORT);
